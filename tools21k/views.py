4cc5fd49068410ec54b1ae303b1dead3f56d5c26
alwaysnithish <inithish21k@gmail.com>
2025-07-21 02:18:34 +0530

    Update views.py

--- tools21k/views.py
+++ tools21k/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+def help(request):
+    return render(request, 'help.html')
+
+def privacypolicy(request):
+    return render(request, 'pp.html')
+
+def termsandconditions(request):
+    return render(request, 'tc.html')
+def about(request):
+    return render(request, 'about.html')
+
+def blog(request):
+    return render(request, 'blog.html')
+
+
