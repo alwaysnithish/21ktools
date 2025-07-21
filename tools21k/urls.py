aa0e01503d71605f396c687e6a8516e0ac523e5d
alwaysnithish <inithish21k@gmail.com>
2025-07-21 16:15:11 +0530

    Update urls.py

--- tools21k/urls.py
+++ tools21k/urls.py
@@ -14,6 +14,7 @@ from django.urls import path, include,re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
