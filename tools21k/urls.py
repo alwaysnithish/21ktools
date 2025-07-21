9ce282bc866b5dee03cedec7d65c23eab615b9f0
alwaysnithish <inithish21k@gmail.com>
2025-07-21 16:06:14 +0530

    Update urls.py

--- tools21k/urls.py
+++ tools21k/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include,repath
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
