3a5bb2f1373bc94be88d10476cf746133984ca1c
alwaysnithish <inithish21k@gmail.com>
2025-08-19 11:56:08 +0530

    Update views.py

--- tools21k/views.py
+++ tools21k/views.py
@@ -26,3 +26,9 @@ def interest(request):
     return render(request, 'interest.html')
 
     
+from django.shortcuts import render
+
+def custom_page_not_found(request, exception):
+    return render(request, '404.html', status=404)
+
+handler404 = custom_page_not_found
