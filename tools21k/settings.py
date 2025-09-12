7fd9e975802f0c0ed3cfa80be4c3de6736d1674e
alwaysnithish <inithish21k@gmail.com>
2025-09-12 12:56:04 +0530

    Update settings.py

--- tools21k/settings.py
+++ tools21k/settings.py
@@ -101,7 +101,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
+
 
 # Media files (uploads)
 # Media Files Configuration
